fa63877ca509f15074e05d968d282e01c2c5dd84
yusa-imit <esimileiar81@gmail.com>
2022-08-04 18:34:51 +0900

    add front into app with react 18 ssr

---
+++ front/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <html>
+      <header></header>
+      <body></body>
+    </html>
+  );
+}
--- server/server.tsx
+++ server/server.tsx
@@ -4,6 +4,9 @@ import { Server } from "socket.io";
 import { getChatHistory } from "../db/util/chatHistory/getChatHistory";
 import { getRooms } from "../db/util/rooms/getRooms";
 import { addTestChatHistory } from "../test/util/DB/addTestChatHistory";
+import ReactDOMServer from "react-dom/server";
+import React from "react";
+import App from "../front/App";
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +14,11 @@ const io = new Server(server);
 /**await insertRooms([
   { id: uuidv4(), name: "test" + uuidv4(), desc: "sample desc", users: [] },
 ]);*/
+
+app.get("/", (req, res) => {
+  ReactDOMServer.renderToPipeableStream(<App />).pipe(res);
+});
+
 const rooms = await getRooms({ logging: true });
 for (const room of rooms) {
   //await addTestChatHistory(room.id);
